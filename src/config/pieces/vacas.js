d1223c0f4fafe6c435cd14262716e10f3d0ea2db
Agustín Santiago <agustin.santiago@eticco.es>
2025-12-30 14:30:10 +0100

    Añade descripciones de Vacuo

--- src/config/pieces/vacas.js
+++ src/config/pieces/vacas.js
@@ -4,191 +4,229 @@
 // - description: Descripción o contexto de la obra
 // - materials: Técnica, materiales, dimensiones...
 // - year: Año de la obra (aparece al final)
+// - order: Número para ordenar manualmente la secuencia (1, 2, 3...)
 
 export const vacasPieces = {
     "vacas_2-0_1": {
+        order: 27,
         title: "",
         description: "",
         materials: "",
         year: ""
     },
     "vacas_2-0_2": {
+        order: 20,
         title: "",
         description: "",
         materials: "",
         year: ""
     },
     "vacas_2-0_3": {
+        order: 17,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-0_4": {
+        order: 4,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-0_5": {
+        order: 5,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-0_6": {
+        order: 6,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_0": {
+        order: 7,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_1": {
+        order: 8,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_2": {
+        order: 9,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_3": {
+        order: 10,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_4": {
+        order: 11,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_5": {
+        order: 12,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_6": {
+        order: 13,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_7": {
+        order: 14,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_8": {
+        order: 15,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-2_9": {
+        order: 16,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_1": {
+        order: 1,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_2": {
+        order: 18,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_3": {
+        order: 19,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_4": {
+        order: 3,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_5": {
+        order: 21,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_6": {
+        order: 22,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_7": {
+        order: 23,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_8": {
+        order: 24,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_9": {
+        order: 25,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_10": {
+        order: 26,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_11": {
+        order: 2,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_12": {
+        order: 28,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_13": {
+        order: 29,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_14": {
+        order: 30,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_15": {
+        order: 31,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_16": {
+        order: 32,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_17": {
+        order: 33,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_18": {
+        order: 34,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_19": {
+        order: 35,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_20": {
+        order: 36,
         title: "",
         description: "",
         materials: ""
     },
     "vacas_2-3_21": {
+        order: 37,
         title: "",
         description: "",
         materials: ""
--- src/config/pieces/vacuo.js
+++ src/config/pieces/vacuo.js
@@ -8,163 +8,259 @@
 
 export const vacuoPieces = {
     "00_vacuo": {
+        order: 1,
         title: "Vacuo",
         description: "Pieza porta-vacíos infinitos",
         materials: "Plata reciclada, tejo, musgo, cerámica, raíz y pintura",
         year: "2022"
     },
     "01_vacuo": {
+        order: 2,
         title: "Coucetsín",
-        description: "La resistencia crea frontera, esa linea invisible entre alguien y nada",
+        description: `
+            La resistencia crea frontera,
+            esa linea invisible entre alguien y nada
+        `,
         materials: "Plata reciclada oxidada, tejo, iroko, boj y pintura",
         year: "2022"
     },
     "02_vacuo": {
+        order: 3,
         title: "Escanda",
-        description: "Con tierra no hay hambre, sin hombre no hay pan, maleza no es trigo",
+        description: `
+            Con tierra no hay hambre,
+            sin hombre no hay pan
+            maleza no es trigo
+        `,
         materials: "Plata reciclada oxidada, cuchara antigua, escanda y cristal",
         year: "2021"
     },
     "03_vacuo": {
+        order: 4,
         title: "Hermética",
-        description: "El esfuerzo brota por los poros, no hay sudor sin trabajo",
+        description: `
+            El esfuerzo brota por los poros,
+            no hay sudor sin trabajo
+        `,
         materials: "Plata reciclada oxidada, tejo y hierro oxidado",
         year: "2021"
     },
     "04_vacuo": {
+        order: 5,
         title: "Hermética",
-        description: "El esfuerzo brota por los poros, no hay sudor sin trabajo",
+        description: `
+            El esfuerzo brota por los poros,
+            no hay sudor sin trabajo
+        `,
         materials: "Plata reciclada oxidada, tejo y hierro oxidado",
         year: "2021"
     },
     "05_vacuo": {
+        order: 6,
         title: "La ciudad hambrienta",
-        description: "No comas la tierra que te da de comer",
+        description: `
+            No comas la tierra
+            que te da de comer
+        `,
         materials: "Plata reciclada oxidada, madera antigua y plástico",
         year: "2020"
     },
     "06_vacuo": {
+        order: 7,
         title: "La ciudad hambrienta",
-        description: "No comas la tierra que te da de comer",
+        description: `
+            No comas la tierra
+            que te da de comer
+        `,
         materials: "Plata reciclada oxidada, madera antigua y plástico",
         year: "2020"
     },
     "07_vacuo": {
+        order: 8,
         title: "De qué sirve contar si no paramos de restar",
-        description: 
-            `Concejo de Tineo: 9096 h.
+        description: `
+            Concejo de Tineo: 9096 h.
             Aldeas y caseríos deshabitados: 33
-            Fuente : Sadei, 2022`,
+            Fuente: Sadei, 2022
+        `,
         materials: "Plata reciclada oxidada, tejo y hierro",
         year: "2020"
     },
     "08_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 9,
+        title: "Coucetsín",
+        description: `
+            La resistencia crea frontera
+            esa linea invisible entre alguien y nada
+        `,
+        materials: "Plata reciclada oxidada, tejo, iroko, boj y pintura",
+        year: "2022"
     },
     "09_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 10,
+        title: "Coucetsín",
+        description: `
+            La resistencia crea frontera
+            esa linea invisible entre alguien y nada
+        `,
+        materials: "Plata reciclada oxidada, tejo, iroko, boj y pintura",
+        year: "2022"
     },
     "10_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 11,
+        title: "Coucetsín",
+        description: `
+            La resistencia crea frontera
+            esa linea invisible entre alguien y nada
+        `,
+        materials: "Plata reciclada oxidada, tejo, iroko, boj y pintura",
+        year: "2022"
     },
     "133_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 12,
+        title: "Escanda",
+        description: `
+            Con tierra no hay hambre,
+            sin hombre no hay pan
+            maleza no es trigo
+        `,
+        materials: "Plata reciclada oxidada, cuchara antigua, escanda y cristal",
+        year: "2021"
     },
     "14_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 13,
+        title: "Días de ferias",
+        description: "'Nin xente nin ganao'",
+        materials: "Plata reciclada, foto antigua, hierro viejo, tejo, ébano y cerezo.",
+        year: "2021"
     },
     "15_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 14,
+        title: "La última gota",
+        description: `
+            No ha de ser la que colme el vaso
+            cerremos el grifo del abandono
+        `,
+        materials: "Plata reciclada oxidada, tejo, cristal y cuarzo",
+        year: "2021"
     },
     "16_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 15,
+        title: "La última gota",
+        description: `
+            No ha de ser la que colme el vaso
+            cerremos el grifo del abandono
+        `,
+        materials: "Plata reciclada oxidada, tejo, cristal y cuarzo",
+        year: "2021"
     },
     "17_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 16,
+        title: "Homo fugit",
+        description: `
+            No abandones la semilla de la naturaleza
+            domada, con el tiempo en contra
+            y el viento a favor
+        `,
+        materials: "Plata reciclada oxidada, cobre patinado, tejo, fibra de coco y pintura",
+        year: "2022"
     },
     "18_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 17,
+        title: "MaTToresu",
+        description: `
+            En este jergón los anhelos sueños son,
+            en mi tierra y en Japón
+        `,
+        materials: "Plata reciclada oxidada, hojas de maíz y tejo",
+        year: "2022"
     },
     "19_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 18,
+        title: "MaTToresu",
+        description: `
+            En este jergón los anhelos sueños son,
+            en mi tierra y en Japón
+        `,
+        materials: "Plata reciclada oxidada, hojas de maíz y tejo",
+        year: "2022"
     },
     "20_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 19,
+        title: "Portón",
+        description: `
+            Puertas que se abren para
+            marchar se cierran para volver
+        `,
+        materials: "Plata reciclada oxidada, tejo y zinc",
+        year: "2020"
     },
     "21_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 20,
+        title: "Galería de los errores",
+        description: `
+            Toda una vida luchando para que al final
+            ganen los perdedores
+        `,
+        materials: "Plata reciclada oxidada, cráneo ratón, foto antigua, papel pintado y tejo",
+        year: "2022"
     },
     "22_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 21,
+        title: "Galería de los errores",
+        description: `
+            Toda una vida luchando para que al final
+            ganen los perdedores
+        `,
+        materials: "Plata reciclada oxidada, cráneo ratón, foto antigua, papel pintado y tejo",
+        year: "2022"
     },
     "23_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 22,
+        title: "Mentiras de colores",
+        description: `
+            Las promesas en las urnas
+            fermentan como el vino malo
+        `,
+        materials: "Plata reciclada oxidada, moneda antigua, ébano, papel electoral y pieza reloj",
+        year: "2020"
     },
     "24_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 23,
+        title: "Mentiras de colores",
+        description: `
+            Las promesas en las urnas
+            fermentan como el vino malo
+        `,
+        materials: "Plata reciclada oxidada, moneda antigua, ébano, papel electoral y pieza reloj",
+        year: "2020"
     },
     "25_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 24,
+        title: "Vacuo",
+        description: "Pieza porta-vacíos infinitos",
+        materials: "Plata reciclada, tejo, musgo, cerámica, raíz y pintura",
+        year: "2022"
     },
     "26_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 25,
+        title: "Artos",
+        description: `
+            Entre los restos inhabitables crece la zarza,
+            de alguna manera vuelve la vida
+        `,
+        materials: "Plata reciclada, tejo, cobre patinado y fibra de coco",
+        year: "2022"
     },
     "27_vacuo": {
-        title: "",
-        description: "",
-        materials: "",
-        year: ""
+        order: 26,
+        title: "Artos",
+        description: `
+            Entre los restos inhabitables crece la zarza,
+            de alguna manera vuelve la vida
+        `,
+        materials: "Plata reciclada, tejo, cobre patinado y fibra de coco",
+        year: "2022"
     }
 };
 
