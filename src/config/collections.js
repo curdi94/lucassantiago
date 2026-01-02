4b89b113ce106ff1082317298064faeedb660f3f
Agustín Santiago <agustin.santiago@eticco.es>
2026-01-02 11:03:35 +0100

    Ajustes en colecciones

--- src/config/collections.js
+++ src/config/collections.js
@@ -10,8 +10,8 @@ export const collections = [
         description: `El continente se deshace y el contenido se diluye. Entre las ruinas deshabitadas y abandonadas aparecen como tesoros sin valor alguno, objetos, útiles ya inútiles, restos como rastros de esa manera de vivir que ya se fue hace mucho tiempo. Retazos de las vivencias de sus moradores que ya no volverán, ni sus hijos, ni los hijos de sus hijos. Así termina una historia que comenzó tiempo atrás, así el desarraigo cae con todo su peso, como un velo de niebla que lo cubre todo y lo hace invisible. Esas piezas sin valor material alguno mutan en nuevos objetos, "joyas", se exhiben en antiguas mesitas donde antaño se guardaba lo más preciado, lo más cercano, los anhelos y quizá los sueños de una vida mejor en otro lugar. Alzan su voz para decirnos que no son objetos vacuos, reivindican nuestra memoria, nuestro no-ólvido y sólo ruegan un momento de atención, un pequeño espacio en nuestros corazones.`
     },
     { folder: 'chic_rural', name: 'Chic Rural', description: '' },
-    { folder: 'galardones', name: 'Galardones', description: 'Galardones realizados por encargo' },
     { folder: 'vacas', name: 'Vacas', description: ''},
+    { folder: 'galardones', name: 'Galardones', description: 'Galardones realizados por encargo' },
     { folder: 'recolectores', name: 'Recolectores', description: '' },
     { folder: 'ciudades_intermitentes', name: 'Ciudades Intermitentes', description: '' },
     { folder: 'por_que_lloran_las_mujeres', name: '¿Por qué lloran las mujeres?', description: '' },
--- src/config/pieces/vacas.js
+++ src/config/pieces/vacas.js
@@ -107,128 +107,330 @@ export const vacasPieces = {
     },
     "vacas_2-3_1": {
         order: 1,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Canción de la Tierra",
+        description: `
+            La tierra nos habla,
+            con su propia letra y su propia música.
+            Cuando las vacas pastan en el prado
+            ¿escucharán la música que emana de la tierra?
+            ¿sonará esta como Mahler?
+            Quizás muchas veces nosotros
+            no seamos capaces de sentir ese sonido.
+        `,
+        materials: "Plata oxidada, madera de tejo, hilo y pintura",
+        year: "Colgante, 2017"
     },
     "vacas_2-3_2": {
         order: 18,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Diapositiva K",
+        description: `
+            En la era de la imagen,
+            las vacas se ponen guapas,
+            recorrerán miles de kilometros
+            por el aire viajando
+            y entrando en nuestras casas
+            sin aromas de campo,
+            como en aquellos viejos almanaques
+            donde los meses van pasando.
+        `,
+        materials: "Plata oxidada, madera de tejo y pintura",
+        year: "Colgante, 2017"
     },
     "vacas_2-3_3": {
         order: 19,
-        title: "",
-        description: "",
-        materials: ""
+        title: "El viaje",
+        description: `
+            Subir en verano ,
+            bajar en invierno,
+            Es el ciclo vital,
+            que año tras año va sucediendo.
+            Trashumantes que ya no sois
+            del valle a la braña,
+            más si, del valle a la urbe
+            buscando los sueños
+            casi nunca cumplidos.
+        `,
+        materials: "Plata oxidada, madera de boj, musgo natural, pintura",
+        year: "Colgante, 2017"
     },
     "vacas_2-3_4": {
         order: 3,
-        title: "",
-        description: "",
-        materials: ""
+        title: "H2O",
+        description: `
+            Gira la noria,
+            vuelta tras vuelta,
+            Castilla se seca
+            de pueblos y gentes.
+            El toro impasible invita a beber,
+            será un mal trago
+            si el agua no brota
+            y afloja la cuerda.
+        `,
+        materials: "Plata oxidada, madera de tejo y pintura",
+        year: "Colgante, 2017"
     },
     "vacas_2-3_5": {
         order: 21,
-        title: "",
-        description: "",
-        materials: ""
+        title: "De colores y banderas...",
+        description: `
+            Dos fuerzas que luchan encontradas,
+            sin sentido y sin razón
+            guiadas por colores iguales que forman banderas distintas.
+            Toro contra vaca,
+            fuerza contra cerrazón
+            que no conduce a nada,
+            tan solo a que el resto del rebaño
+            acabe soñando en blanco y negro,
+            donde no existan los colores
+            y las banderas sean iguales.
+        `,
+        materials: "Plata patinada, plata oxidada e hilo | Plata dorada e hilo",
+        year: "Colgantes 2018"
     },
     "vacas_2-3_6": {
         order: 22,
-        title: "",
-        description: "",
-        materials: ""
+        title: "De colores y banderas...",
+        description: `
+            Dos fuerzas que luchan encontradas,
+            sin sentido y sin razón
+            guiadas por colores iguales que forman banderas distintas.
+            Toro contra vaca,
+            fuerza contra cerrazón
+            que no conduce a nada,
+            tan solo a que el resto del rebaño
+            acabe soñando en blanco y negro,
+            donde no existan los colores
+            y las banderas sean iguales.
+        `,
+        materials: "Plata patinada, plata oxidada e hilo | Plata dorada e hilo",
+        year: "Colgantes, 2018"
     },
     "vacas_2-3_7": {
         order: 23,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Meteoro",
+        description: `
+            De repente algo ocurre, sorpresivamente,
+            al principio nos quedamos traspuestos
+            aunque aún no alcancemos a imaginar
+            como ese cambio transformará nuestras vidas,
+            es como la caída de un meteorito,
+            como abrir un viejo sobre-sorpresa.
+        `,
+        materials: "Plata reciclada oxidada, meteorito y madera de tejo",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_8": {
         order: 24,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Como la cabeza al sombrero",
+        description: `
+            Como lo blanco a lo negro.
+            Para que no caliente la cabeza,
+            para que no se enífrien las ideas.
+            Porque la leche es blanca
+            y la noche gris.
+        `,
+        materials: "Plata reciclada",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_9": {
         order: 25,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Nube | Cloud",
+        description: `
+            Cerrado en una habitación,
+            cubículo secreto ajeno a las miradas,
+            Cada ser rumia sus anhelos
+            que se escapan por un hilo,
+            no están allí, están en otro lugar,
+            en el aire, en el espacio, en la nube.
+        `,
+        materials: "Plata reciclada oxidada, madera y pintura",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_10": {
         order: 26,
-        title: "",
-        description: "",
-        materials: ""
+        title: "La voz de tu amo",
+        description: `
+            Como grabado en un viejo vinilo
+            repite siempre las mismas palabras,
+            el mismo mantra,
+            y lo lanza al aire, sin remordimientos,
+            sin pararse a pensar cuantos hay como él
+            y que lo único que cambia es el disco.
+        `,
+        materials: "Plata reciclada oxidada y disco vinilo",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_11": {
         order: 2,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Timeless",
+        description: `
+            Ya se hace tarde y cada vez queda menos,
+            se escucha un susurro,
+            agónico y desapercibido.
+            Cuando las cadenas del tiempo
+            ya no puedan mover tanta desidia
+            el viejo cu-cú ya no reclamará nuestra atención,
+            ya no habrá tiempo.
+        `,
+        materials: "Plata reciclada oxidada, plata dorada, madera de tejo y olmo, pieza antigua de reloj y seoteh-brite",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_12": {
         order: 28,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Box",
+        description: `
+            Todo cabe en una caja
+            tan solo depende de su tamaño,
+            un martillo,
+            un sueño roto,
+            un aroma,
+            un sentimiento
+            y un principio y un final.
+            Una vaca tiene origen,
+            ¿y destino?
+        `,
+        materials: "Plata reciclada, cartón reciclado y acero",
+        year: "Broche, 2017"
     },
     "vacas_2-3_13": {
         order: 29,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Box",
+        description: `
+            Bebé que nace,
+            bebe que bebe,
+            falsa leche
+            donde la vida no crece,
+            Es una mentira,
+            luchar por eso
+            ya no merece.
+        `,
+        materials: "Plata reciclada oxidada, cobre, hilo y pintura",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_14": {
         order: 30,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Box",
+        description: `
+            Cae la lluvia con fuerza inusitada,
+            esta vez no transparente,
+            blanquecina como leche aguada.
+            Tras la tormenta imperfecta,
+            siempre habrá presto un arco iris
+            que cubra esta mentira,
+            como el telón de un viejo teatro,
+            en la obra mil veces repetida.
+        `,
+        materials: "Plata reciclada oxidada, madera de tejo y bambú, pintura y colorante",
+        year: "Colgante, 2019"
     },
     "vacas_2-3_15": {
         order: 31,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Papeles y más papeles",
+        description: `
+            Cuando la burocracia entra por la puerta
+            la razón salta por la ventana.
+            Cada vez más papeles que no entendemos,
+            como en lengua extraña,
+            para crear problemas que no existen,
+            para marcar lineas rojas
+            que nos muestren la frontera,
+            entre el cielo y el suelo,
+            entre la razón y la ley.
+        `,
+        materials: "Plata reciclada oxidada, madera de tejo, madera antigua, periódico e hilo.",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_16": {
         order: 32,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Akira",
+        description: `
+            La tradición es el progreso.
+            Debemos usar el pasado para afianzar el futuro,
+            desde donde los que vuelven
+            reclaman el no-olvido,
+            aquel que nunca debieron
+            dejar atrás.
+        `,
+        materials: "Plata reciclada oxidada, madera de tejo, hierba seca e hilo",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_17": {
         order: 33,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Hormigón",
+        description: `
+            Sube la marea
+            y el hormigón cubre las playas de nuestros pueblos,
+            sumergiendo el origen.
+            Una vieja madera quedará flotando
+            y evitará el anogo,
+            ayudándonos a navegar
+            hasta que de nuevo encalle.
+        `,
+        materials: "Plata reciclada oxidada, hormigón y madera de tejo",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_18": {
         order: 34,
-        title: "",
-        description: "",
-        materials: ""
+        title: "La sombra que no cesa",
+        description: `
+            Buscando la sombra
+            encuentras la calma,
+            serenidad que no apabulla,
+            reflexión.
+            Sombra gris de elemento verde,
+            Llega la paz y se instala el silencio,
+            demasiadas preguntas
+            pero muchas respuestas.
+            Llega la luz.
+        `,
+        materials: "Plata reciclada oxidada, fibra de coco, madera de tejo, pintura y cobre",
+        year: "Broche-colgante, 2018"
     },
     "vacas_2-3_19": {
         order: 35,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Transgenic",
+        description: `
+            Modificamos la vida,
+            la naturaleza que nos envuelve,
+            y como un nuevo ropaje
+            nos disfraza de lo que no somos,
+            de lo que frecuentemente no queremos ser,
+            como un espejismo que se desvanece
+            cuando el génesis ya no nos pertenece,
+            ya no está aquí,
+            ya no germinará.
+        `,
+        materials: "Plata reciclada oxidada, madera quemada, hoja de maíz natural, acero y colorante",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_20": {
         order: 36,
-        title: "",
-        description: "",
-        materials: ""
+        title: "No es oro",
+        description: `
+           Palabras y promesas
+           que relucen como el oro aunque no lo sean.
+           Como un sueño que nos atrapa, una esperanza,
+           Vemos nuestro reflejo y solo es brillo,
+           falsa luz que sustenta el árbol
+           donde, de nuevo, brotará salvaje la vida.
+        `,
+        materials: "Plata reciclada oxidada, pirita natural, madera de boj, hilo y pintura",
+        year: "Colgante, 2018"
     },
     "vacas_2-3_21": {
         order: 37,
-        title: "",
-        description: "",
-        materials: ""
+        title: "Sedimentos",
+        description: `
+            Bajo los escombros de un futuro ideal,
+            se muestra arrogante un pasado sincero,
+            nunca perfecto, más siempre útil,
+            sobremanera real.
+            Es la herramienta que encaja
+            en la pieza que perdura
+            y nos enseña tranquilo
+            todo un mundo entero.
+        `,
+        materials: "Plata reciclada oxidada, disipador aluminio reciclado,  madera de tejo, cuerno, terracota, latón, cobre y bambú.",
+        year: "Colgante-broche, 2019"
     }
 };
